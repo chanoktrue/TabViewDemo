758c191ca57f5d2b231566436f3104ed64b7d69a
chanoktrue <chanoktrue@me.com>
2024-01-04 05:45:31 +0700

    Initial Commit

---
+++ TabViewDemo/TabViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewDemoApp.swift
+//  TabViewDemo
+//
+//  Created by Thongchai Subsaidee on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
